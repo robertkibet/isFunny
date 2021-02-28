3ad9be6fee52406d84c5378d617a8ff2aebb4245
Robert Kibet <kipronokrobert@gmail.com>
2021-02-28 17:37:36 +0300

    added header section

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Jokes } from './src/views/jokes';
-import { containerStyles } from './src/styles';
+import { colors, containerStyles } from './src/styles';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Icon } from 'react-native-elements';
 
+const Header = () => {
+  return (
+    <View style={containerStyles.header}>
+      <Icon
+        type="ionicon"
+        name="menu-outline"
+        color={colors.dark}
+        size={30}
+        onPress={() => { }}
+      />
+    </View>
+  )
+}
 export default function App() {
   return (
-    <View style={containerStyles.index}>
-      <View style={containerStyles.wrapper}>
-        <Jokes />
+    <SafeAreaProvider>
+      <View style={containerStyles.index}>
+        <View style={containerStyles.wrapper}>
+          <Header />
+          <Jokes />
+        </View>
       </View>
-    </View>
+    </SafeAreaProvider>
   );
 }
--- src/styles/containers.tsx
+++ src/styles/containers.tsx
@@ -1,35 +1,46 @@
 import { StyleSheet, Dimensions } from 'react-native';
 import { colors } from './global/colors-enums';
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 export const containerStyles = StyleSheet.create({
   landing: {
-    height:(height-40),
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    paddingTop:200
+    height: (height - 40),
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 200
   },
   index: {
     // padding: 20,
     // flex: 1,
     backgroundColor: colors.body.background.dark,
     position: 'relative',
-    fontFamily: 'Josefin Sans'
+    fontFamily: 'Josefin Sans',
+    width: width
   },
-  wrapper:{
-    borderTopRightRadius:300,
+  wrapper: {
+    borderTopRightRadius: 300,
     padding: 20,
     backgroundColor: colors.body.background.light,
   },
-  buttonBottom:{
+  buttonBottom: {
     flex: 1,
     justifyContent: 'flex-end',
   },
-  shareIcons:{
-    display:'flex',
-    flexDirection:'row',
-    justifyContent:'space-evenly',
-    width:'50%'
+  shareIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '50%'
+  },
+  header: {
+    height: 40,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    paddingLeft: 20,
+    paddingTop: 20
   }
 })
